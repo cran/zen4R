2a24e0352357865d43e0b29920044b17afc2838a
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2022-02-16 15:30:02 +0000

    version 0.5-3

--- R/zen4R_logger.R
+++ R/zen4R_logger.R
@@ -5,37 +5,41 @@
 #' @keywords logger
 #' @return Object of \code{\link{R6Class}} for modelling a simple logger
 #' @format \code{\link{R6Class}} object.
-#'
-#' @section Abstract Methods:
-#' \describe{
-#'  \item{\code{INFO(text)}}{
-#'    Logger to report information. Used internally
-#'  }
-#'  \item{\code{WARN(text)}}{
-#'    Logger to report warnings. Used internally
-#'  }
-#'  \item{\code{ERROR(text)}}{
-#'    Logger to report errors. Used internally
-#'  }
-#' }
 #' 
 #' @note Logger class used internally by zen4R
 #'
 zen4RLogger <-  R6Class("zen4RLogger",
   public = list(
-    #logger
+    #' @field verbose.info logger info status
     verbose.info = FALSE,
+    #' @field verbose.debug logger debug status
     verbose.debug = FALSE,
+    #' @field loggerType Logger type, either "INFO", "DEBUG" or NULL (if no logger)
     loggerType = NULL,
+    
+    #' @description internal logger function for the Zenodo manager
+    #' @param type logger message type, "INFO", "WARN", or "ERROR"
+    #' @param text log message
     logger = function(type, text){
       if(self$verbose.info){
         cat(sprintf("[zen4R][%s] %s - %s \n", type, self$getClassName(), text))
       }
     },
+    
+    #' @description internal INFO logger function
+    #' @param text log message
     INFO = function(text){self$logger("INFO", text)},
+    
+    #' @description internal WARN logger function
+    #' @param text log message
     WARN = function(text){self$logger("WARN", text)},
+    
+    #' @description internal ERROR logger function
+    #' @param text log message
     ERROR = function(text){self$logger("ERROR", text)},
     
+    #' @description initialize the Zenodo logger
+    #' @param logger logger type NULL, 'INFO', or 'DEBUG'
     initialize = function(logger = NULL){
       
       #logger
@@ -55,12 +59,14 @@ zen4RLogger <-  R6Class("zen4RLogger",
       }
     },
     
-    #getClassName
+    #'@description Get object class name
+    #'@return the class name, object of class \code{character}
     getClassName = function(){
       return(class(self)[1])
     },
     
-    #getClass
+    #'@description Get object class
+    #'@return the class, object of class \code{R6}
     getClass = function(){
       class <- eval(parse(text=self$getClassName()))
       return(class)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://github.com/eblondel/zen4R/actions/workflows/r-cmd-check.yml/badge.svg?branch=master)](https://github.com/eblondel/zen4R/actions/workflows/r-cmd-check.yml)
 [![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/zen4R)](https://cran.r-project.org/package=zen4R)
-[![Github_Status_Badge](https://img.shields.io/badge/Github-0.5--2-blue.svg)](https://github.com/eblondel/zen4R)
+[![Github_Status_Badge](https://img.shields.io/badge/Github-0.5--3-blue.svg)](https://github.com/eblondel/zen4R)
 [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.2547036.svg)](https://doi.org/10.5281/zenodo.2547036)
 
 **R Interface to Zenodo REST API**
@@ -13,9 +13,19 @@ Please check the [online documentation](https://github.com/eblondel/zen4R/wiki) 
 
 ## Sponsors
 
-Many thanks to the following organizations that have provided fundings for strenghtening the ``zen4R`` package:
+Many thanks to the following organizations that have provided fundings for strenghtening the ``geoflow`` package:
 
+<div style="float:left;">
+  <a href="https://www.fao.org/home/en/"><img height=200 width=200 src="https://www.fao.org/fileadmin/templates/family-farming-decade/images/FAO-IFAD-Logos/FAO-Logo-EN.svg">
 <a href="https://en.ird.fr/"><img src="https://en.ird.fr/sites/ird_fr/files/2019-08/logo_IRD_2016_BLOC_UK_COUL.png" height=200 width=200/></a>
+</div>
+
+The following projects have contributed to strenghten ``zen4R``:
+
+<a href="https://www.blue-cloud.org"><img height=100 width=300 src="https://www.blue-cloud.org/sites/all/themes/arcadia/logo.png"/></a>
+
+_Blue-Cloud has received funding from the European Union's Horizon programme call BG-07-2019-2020, topic: [A] 2019 - Blue Cloud services, Grant Agreement No.862409._
+
 
 For zen4R sponsoring/funding new developments, enhancements, support requests, please contact me by [e-mail](mailto:eblondel.pro@gmail.com)
 
